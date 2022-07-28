92433753a665fd92536958a0eaff97074468b40f
Hammad <hmdshfq@outlook.com>
2022-07-28 21:38:21 +0200

    add youtube api

---
+++ src/apis/youtube.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: import.meta.env.VITE_API_KEY
+  },
+})

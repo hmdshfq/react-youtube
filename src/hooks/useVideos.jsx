1c04c28e7590324eacd4303041d6f3f14f8350a3
Hammad <hmdshfq@outlook.com>
2022-07-28 23:21:07 +0200

    create custom hook

---
+++ src/hooks/useVideos.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import youtube from '../apis/youtube';
+
+const useVideos = defaultSearchTerm => {
+  const [videos, setVideos] = useState([]);
+
+  const onSearchSubmit = async value => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: value,
+      },
+    });
+    setVideos(response.data.items);
+  };
+
+  useEffect(() => {
+    search(defaultSearchTerm);
+  }, []);
+
+  /* 
+  We can return the values in two ways 
+  Either use the React convention and use
+  return [videos, search]
+  or use the JS convention and use
+  return { videos, search }
+  */
+  return [videos, search];
+};
+
+export default useVideos;
